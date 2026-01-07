b61dec17b2e65a9752a0681781220c5fa35e5061
redasbar <reda.sbar@gmail.com>
2026-01-07 10:10:50 -0500

    entrypoint for migrate container

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -6,11 +6,6 @@ until pg_isready -h db -U "$DB_USER"; do
   sleep 2
 done
 
-# echo "Applying database migrations..."
-# export MIGRATION_DATABASE_URL="postgresql://miguser:Sdlgih865qfkjdmqDoOàSf84Fzse1Li8@db:5432/LibraTech_db"
-# flask db upgrade
-# unset MIGRATION_DATABASE_URL
-
 # Start Gunicorn
 echo "Starting Gunicorn..."
 exec gunicorn --workers 3 --bind 0.0.0.0:8000 wsgi:app
